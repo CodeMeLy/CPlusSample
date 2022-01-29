b926273afac1e244ecada4d1799fe91212943b10
tteam1234 <phithaitam@gmail.com>
2022-01-29 19:13:35 +0700

    transform trong thu vien algorithm

--- Vector/Vector001.cpp
+++ Vector/Vector001.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <algorithm>// transform
 #include<sstream>
 #include <vector>
 using namespace std;
+int op_increase(int i){return ++i;}
 int main(){
     vector<int> vector;
     std::vector<int> vector2(10);
@@ -11,6 +13,11 @@ int main(){
     copy(vector.begin(), vector.end(),vector2.begin());
     for(int i=0; i< 10; i++){
         cout<<vector[i]<<" ";
+    }
+    std::transform(vector.begin(),vector.end(),vector.begin(),op_increase);
+    cout<<endl;
+    for(int i:vector){
+        cout<<i<<" ";
     }
     return 0;
 }
