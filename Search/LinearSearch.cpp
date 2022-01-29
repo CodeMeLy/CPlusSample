1b04fe32d7ec5e9e980fcc54b6d8d4d5e5126577
tteam1234 <phithaitam@gmail.com>
2022-01-29 14:52:24 +0700

    kiem tra tuan suat xuat hien cua x trong mang

--- Search/LinearSearch.cpp
+++ Search/LinearSearch.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 bool solution1(int a[], int n, int x);// kiểm tra xem x có trong mảng hay không?
+int solution2(int a[],int n, int x);// tìm tần suất xuất hiện của x trong mảng
 int main(){
     int x = 3;// x là giá trị cần tìm
-    int a[] = {1,2,3,4,5};
+    int a[] = {1,2,3,4,5,3,3,3,3};
     int n = sizeof(a) / sizeof(a[0]);
     if(solution1(a,n,x)){
-        cout<<x<<" có tồn tại trong mảng"<<endl;
+        cout<<x<<" tồn tại "<<solution2(a,n,x)<<" lần trong mảng"<<endl;
     } else{
         cout<<x<<" không tồn tại trong mảng"<<endl;
     }
@@ -21,4 +22,13 @@ bool solution1(int a[], int n, int x) {
         }
     }
     return hasX;
+}
+int solution2(int a[], int n, int x){
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] == x){
+            count++;
+        }
+    }
+    return count;
 }
