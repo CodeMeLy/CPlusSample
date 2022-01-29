c01bc54d24b2a20ee0c2d67538991fd50b508a98
tteam1234 <phithaitam@gmail.com>
2022-01-29 14:33:01 +0700

    nhap xuat point

--- Struct/Point.cpp
+++ Struct/Point.cpp
@@ -3,10 +3,22 @@ using namespace std;
 typedef struct Point{
     int x,y;
 };
+void setData(Point &_point, int x, int y){
+    _point.x = x;
+    _point.y = y;
+}
+void enter(Point &_point){
+    cout<<endl<<"x,y: ";
+    cin>>_point.x>>_point.y;
+}
+void print(Point _point){
+    cout<<"("<<_point.x<<","<<_point.y<<")";
+}
 int main(){
     Point point;
-    point.x = 5;
-    point.y = 6;
-    cout<<"("<<point.x<<","<<point.y<<")";
+    setData(point,3,4);
+    print(point);
+    enter(point);
+    print(point);
     return 0;
 }
