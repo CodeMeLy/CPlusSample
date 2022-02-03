f87514042cd13475991246e3d508c9f26668dbca
tteam1234 <phithaitam@gmail.com>
2022-02-03 15:38:46 +0700

    add class Rectangle ke thua tư class Shape

--- OOP/Shape.cpp
+++ OOP/Shape.cpp
@@ -8,7 +8,16 @@ class Shape{
             return getSurface() * 2;
         };
 };
+class Rectangle:Shape{
+    private:
+        float width, height;
+    public:
+        Rectangle(){};
+        void description() const {std::cout<<"This is Rectangle."<<std::endl;}
+        double getSurface() const {return width*height;}
+};
 int main(){
-
+    Rectangle rectangle = Rectangle();
+    rectangle.description();
     return 0;
 }
